9755e4a338a6dc7ddd1ba12a93cb9f6e05ad4cc5
tasveer-fatama <tasveerfatema135@gmail.com>
2024-11-28 18:33:35 +0530

    ......

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Signup from './Signup'
+import Login from './Login'
+import { BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <Signup/>  
+    <Router>
+<Routes>
+    <Route path='/'  element={<Login/>}/>
+    <Route path='/signup'  element={<Signup/>}/>
+</Routes>
 
-    </div>
+
+    </Router>
+     
+
+   
   )
 }
 
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,44 +1,28 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
-const Signup = () => {
+const Login = () => {
     const initialState ={
-        name:"",
         email:"",
         password:"",
-        cpassword:"",
     }
-    const [signup,setSignup]= useState(initialState);
+    const [login,setLogin]= useState(initialState);
     const inputHandler =(e)=>{
       // console.log('Name:', e.target.name, 'Value:', e.target.value);
-        setSignup({...signup, [e.target.name]: e.target.value});
+        setLogin({...login, [e.target.name]: e.target.value});
     }
     const submitHandler =(e)=>{
         e.preventDefault();
-        console.log(signup);
+        console.log(login);
         
     }
   return (
-    <div className='flex justify-center items-center  h-screen'>
+    <div>
+       <div className='flex justify-center items-center  h-screen'>
       <div className="w-96 backdrop-blur-lg bg-opacity-80 rounded-lg shadow-lg p-5 bg-gray-900 text-white">
-  <h2 className="text-2xl font-bold pb-5">SignUp</h2>
+  <h2 className="text-2xl font-bold pb-5">Login</h2>
   <form onSubmit={submitHandler}>
-    <div className="mb-4">
-      <label htmlFor="name" className="block mb-2 text-sm font-medium">
-        Your name
-      </label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        onChange={inputHandler}
-        className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
-        placeholder="Andrew Jackson"
-        required=""
-        defaultValue=""
-      />
-    </div>
-    <div className="mb-4">
+   <div className="mb-4">
       <label htmlFor="email" className="block mb-2 text-sm font-medium">
         Your email
       </label>
@@ -46,7 +30,7 @@ const Signup = () => {
         type="email"
         id="email"
         name="email"
-        onChange={inputHandler}
+         onChange={inputHandler}
         className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
         placeholder="andrew@mail.com"
         required=""
@@ -68,21 +52,7 @@ const Signup = () => {
         defaultValue=""
       />
     </div>
-    <div className="mb-4">
-      <label htmlFor="password" className="block mb-2 text-sm font-medium">
-        Confirm password
-      </label>
-      <input
-        type="cpassword"
-        id="cpassword"
-        name="cpassword"
-        onChange={inputHandler}
-        className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
-        placeholder="*********"
-        required=""
-        defaultValue=""
-      />
-    </div>
+  
     <div>
       <p className="text-red-500 pb-5" />
     </div>
@@ -91,19 +61,20 @@ const Signup = () => {
         type="submit"
         className="text-white bg-purple-600 hover:bg-purple-700 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-sm py-2.5 px-5 w-full sm:w-auto"
       >
-        SignUp
+        Login
       </button>
       <div className="flex items-center text-sm">
-        <p>Already have an account?</p>
-        <Link to="/">  <p className="underline cursor-pointer ml-1">Login</p></Link>
-      
+        <p>Don't have an account?</p>
+        <Link to="/signup"> <p className="underline cursor-pointer ml-1">SignUp</p></Link>
+       
       </div>
     </div>
   </form>
 </div>
 
     </div>
+    </div>
   )
 }
 
-export default Signup
+export default Login
